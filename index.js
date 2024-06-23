eeec15f8e4889f231695f408b4b2d023a7be8987
Maisha <maisha.mir2014@gmail.com>
2024-06-23 02:38:47 -0700

    created functionality for accessing video by id

--- index.js
+++ index.js
@@ -10,49 +10,54 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-app.use('/images', express.static('public/images'))
+app.use("/images", express.static("public/images"));
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+  console.log(`Server started on port ${PORT}`);
+});
 
 const readVideoFile = () => {
-    try {
-        const videoFile = fs.readFileSync('./data/videos.json');
-        const videoData = JSON.parse(videoFile);
-        return videoData;
-    } catch (e) {
-        console.log("Could not read videos.json", e)
-    }
-}
+  try {
+    const videoFile = fs.readFileSync("./data/videos.json");
+    const videoData = JSON.parse(videoFile);
+    return videoData;
+  } catch (e) {
+    console.log("Could not read videos.json", e);
+  }
+};
 
-app.get('/videos', (req, res) => {
-    try {
-        const videoData = readVideoFile();
-        const strippedVideoData = videoData.map((video, index) => ({
-            id: video.id,
-            title: video.title,
-            channel: video.channel,
-            image: `images/image${index}.jpg`
-        }))
-        res.json(strippedVideoData)
-    } catch (e) {
-        console.error("Unable to handle GET /videos request: ", e);
-        res.status(500).json({message: "Internal Server Error"})
-    }
-})
+app.get("/videos", (req, res) => {
+  try {
+    const videoData = readVideoFile();
+    const strippedVideoData = videoData.map((video, index) => ({
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: `images/image${index}.jpg`,
+    }));
+      res.json(strippedVideoData);
+      console.log(strippedVideoData)
+  } catch (e) {
+    console.error("Unable to handle GET /videos request: ", e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-app.get('/videos/:id', (req, res) => {
-    try {
-        const { id } = req.params;
-        const videoData = readVideoFile();
-        const video = videoData.filter(video => video.id == id)
-        if (video.length === 0) {
-            res.status(404).json({"message": "No video with that id exists"})
-        }
-        res.json(video)
-    } catch (e) {
-        console.error("Unable to handle GET /videos/:id request", e)
-        res.status(500).json({message: "Internal Server Error"})
+app.get("/videos/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const videoData = readVideoFile();
+    const video = videoData.find((video) => video.id == id);
+    if (!video) {
+      res.status(404).json({ message: "No video with that id exists" });
+    } else {
+      const vidIndex = videoData.indexOf(video);
+      video.image = `images/image${vidIndex}.jpg`;
+      res.json(video);
     }
-})
+    console.log(video);
+  } catch (e) {
+    console.error("Unable to handle GET /videos/:id request", e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
